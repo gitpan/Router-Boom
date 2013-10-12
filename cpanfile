269e2bb7844d0bdfcb7797eef7ef2214f8ce1554
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2013-10-12 03:10:46 +0000

    Import of TOKUHIROM/Router-Boom-0.04 from CPAN.
    gitpan-cpan-distribution: Router-Boom
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         TOKUHIROM/Router-Boom-0.04.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- cpanfile
+++ cpanfile
@@ -1,5 +1,5 @@
 requires 'perl', '5.008001';
-requires 'Moo', '1.003001';
+requires 'Class::Accessor::Lite';
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
--- lib/Router/Boom/Node.pm
+++ lib/Router/Boom/Node.pm
@@ -4,23 +4,31 @@ use warnings;
 use utf8;
 use 5.008_001;
 
-use Moo;
-has key => (is => 'ro', required => 1);
-has children => ( is => 'ro', default => sub { +[] } );
-has leaf => ( is => 'rw' );
-no Moo;
+use Class::Accessor::Lite 0.05 (
+    rw => [qw(leaf)],
+    ro => [qw(key children)],
+);
+
+sub new {
+    my $class = shift;
+    my %args = @_==1 ? %{$_[0]} : @_;
+    bless {
+        children => +[],
+        %args,
+    }, $class;
+}
 
 use re 'eval';
 
 sub add_node {
     my ($self, $child) = @_;
-    for (my $i=0; $i<@{$self->children}; $i++) {
-        if ($self->children->[$i]->key eq $child) {
-            return $self->children->[$i];
+    for (my $i=0; $i<@{$self->{children}}; $i++) {
+        if ($self->{children}->[$i]->{key} eq $child) {
+            return $self->{children}->[$i];
         }
     }
-    push @{$self->children}, Router::Boom::Node->new(key => $child);
-    return $self->children->[-1];
+    push @{$self->{children}}, Router::Boom::Node->new(key => $child);
+    return $self->{children}->[-1];
 }
 
 =pod
